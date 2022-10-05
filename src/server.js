141650ba193a9226c92d0ac2d70765784e2fc3d6
04wakeup <42279557+04wakeup@users.noreply.github.com>
2022-10-04 19:54:24 -0700

    socketIO installed

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -10,25 +10,28 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Annoymous";
-  console.log("Connected to Browser");
-  socket.on("close", () => console.log("Disconnected from Browser !"));
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
-  });
-  // socket.send("Hello!");
+wsServer.on("connection", (socket) => {
+  console.log(socket);
 });
-server.listen(3000, handleListen);
+
+// wss.on("connection", (socket) => {
+// const wss = new WebSocket.Server({ server });
+//   sockets.push(socket);
+//   socket["nickname"] = "Annoymous";
+//   console.log("Connected to Browser");
+//   socket.on("close", () => console.log("Disconnected from Browser !"));
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+//   });
+//   // socket.send("Hello!");
+// });
+httpServer.listen(3000, handleListen);
